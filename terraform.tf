14371bd895d92c25dff0518bfd8a0273e548b7a0
Robin Shen <robin@onedev.io>
2024-05-24 09:31:06 +0800

    Removed cloud block

--- terraform.tf
+++ terraform.tf
@@ -2,15 +2,15 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
-
+  /*
   cloud {
-    organization = "organization-name"
+    organization = "1dev"
 
     workspaces {
-      name = "learn-terraform"
+      name = "learn"
     }
   }
-
+  */
   required_providers {
     aws = {
       source  = "hashicorp/aws"
